41e3e85cd622123ec61fc4efc70abaaa3541f7fc
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 19:53:28 -0300

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,26 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Start");
-
+		String[] carros =  {"civic", "gol", "palio", "uno"};
+		
+		for(String carro : carros) {
+		System.out.println("carro:" + carro);
+		
+		}
+		
+		String[] carros1 =  {"civic", "L200", "palio", "uno"};
+		
+		for(String carro : carros1) {
+			System.out.println("carro:" + carro);
+			
+			}
+		
+		String[] carros2 =  {"civic", "L200", "palio", "uno", "gol"};
+		
+		for(String carro : carros2) {
+			System.out.println("carro:" + carro);
+			
+			}
 	}
 
 }
